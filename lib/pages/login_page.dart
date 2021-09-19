c126b3d44559153a0bc5e580590924ed549f579b
Aman Khan <aman7kh8an6@gmail.com>
2021-09-19 12:25:18 +0530

    day 2 commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,17 @@
+// ignore_for_file: prefer_const_constructors;
 import 'dart:convert';
-
 import 'package:catalog_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool onChanged = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -15,13 +22,13 @@ class LoginPage extends StatelessWidget {
               Image.asset(
                 "assets/images/login_image.png",
                 fit: BoxFit.cover,
-                height: 250,
+                height: 200,
               ),
               SizedBox(
                 height: 20.0,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
               SizedBox(
@@ -35,6 +42,10 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter username", labelText: "Username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -44,13 +55,48 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40.0,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          onChanged = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    )
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: onChanged ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: onChanged
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          // shape:
+                          //     onChanged ? BoxShape.circle : BoxShape.rectangle,
+                          borderRadius: onChanged
+                              ? BorderRadius.circular(50)
+                              : BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    // )
                   ],
                 ),
               )
